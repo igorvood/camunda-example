deedadf9626330aec8eeef887c94da2f39a8e7f5
vood <balitsky_igor@list.ru>
2019-12-08 21:27:15 +0300

    initial

---
+++ src/main/kotlin/ru/vood/camundaexample/CamundaExampleApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.camundaexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamundaExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<CamundaExampleApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/camundaexample/CamundaExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.camundaexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamundaExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
